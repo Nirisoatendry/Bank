6d8116ab42de11526d433db26e9eeffcc9a38d87
Rakotondrainibe Nirisoa Tendry <tendrinirisoa@gmail.com>
2022-05-24 16:50:46 +0300

    Component sidemenu

--- src/app/pages/page-layout/page-layout.module.ts
+++ src/app/pages/page-layout/page-layout.module.ts
@@ -5,12 +5,14 @@ import { PageLayoutRoutingModule } from './page-layout-routing.module';
 import { PageLayoutComponent } from './page-layout.component';
 import { HeaderComponent } from 'src/app/features/header/header.component';
 import { PageLoanModule } from '../page-loan/page-loan.module';
+import { SidemenuComponent } from 'src/app/features/sidemenu/sidemenu.component';
 
 
 @NgModule({
   declarations: [
     PageLayoutComponent,
-    HeaderComponent
+    HeaderComponent,
+    SidemenuComponent
   ],
   imports: [
     CommonModule,
